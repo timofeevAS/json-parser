e9332e30d8c660accc7a64a49553503c01cf4e2f
timofeevAS <a.timoff3@gmail.com>
2024-04-17 13:20:21 +0300

    On JC1: add javacc files

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void test(){
+        assertEquals(1,1);
+    }
+}
