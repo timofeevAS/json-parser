1ae3bcc0a7943f8726f206bf6c60da060ae69940
timofeevAS <a.timoff3@gmail.com>
2024-04-21 17:41:01 +0300

    On JC1: example in main

--- src/main/java/ru/timofeevAS/Main.java
+++ src/main/java/ru/timofeevAS/Main.java
@@ -1,7 +1,24 @@
 package ru.timofeevAS;
 
+import ru.timofeevAS.pojos.Car;
+import ru.timofeevAS.pojos.Dealer;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String filepath = "src/main/resources/dealer.json";
+
+        try {
+            String content = Files.readString(Paths.get(filepath), StandardCharsets.UTF_8);
+
+            Dealer dealer = ParserAdapter.parseToClass(content, Dealer.class);
+            System.out.println(dealer);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
